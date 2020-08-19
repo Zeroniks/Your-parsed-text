3244cb84875f6280da42d0817f8511d24ee7096d
Zeroniks <57189702+Zeroniks@users.noreply.github.com>
2020-08-19 18:48:19 +0700

    уху!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<!--
+
     var message="Привет Диман!"
     var neonbasecolor="#990000"
     var neontextcolor="red"
@@ -9,32 +9,31 @@
     var flashpause=1000
     var n=0
     if (document.all||document.getElementById){
-       document.write('<font color="'+neonbasecolor+'" size=+2>')
-       for (m=0;m<message.length;m++)
-    	document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
-       document.write('</font>')
+        document.write('<font color="'+neonbasecolor+'" size=+2>')
+          for (m=0;m<message.length;m++)
+    	     document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
+           document.write('</font>')
     }
     else
        document.write(message)
     function crossref(number){
        var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number)
-       return crossobj
+          return crossobj
     }
     function neon(){
-       //Change all letters to base color
-       if (n==0){
-    	for (m=0;m<message.length;m++)
-    	crossref(m).style.color=neonbasecolor
-    	}
+          if (n==0){
+    	       for (m=0;m<message.length;m++)
+    	        crossref(m).style.color=neonbasecolor
+    }
        crossref(n).style.color=neontextcolor
-       if (n>flashingletters-1) crossref(n-flashingletters).style.color=neontextcolor2
-       if (n>(flashingletters+flashingletters2)-1) crossref(n-flashingletters-flashingletters2).style.color=neonbasecolor
-       if (n<message.length-1) n++
+          if (n>flashingletters-1) crossref(n-flashingletters).style.color=neontextcolor2
+          if (n>(flashingletters+flashingletters2)-1) crossref(n-flashingletters-flashingletters2).style.color=neonbasecolor
+          if (n<message.length-1) n++
        else{
-    	n=0
-    	clearInterval(flashing)
-    	setTimeout("beginneon()",flashpause)
-    	return
+    	     n=0
+    	       clearInterval(flashing)
+          	setTimeout("beginneon()",flashpause)
+          	return
     	}
     }
     function beginneon(){
@@ -42,8 +41,4 @@
        flashing=setInterval("neon()",flashspeed)
     }
     beginneon()
->
-<!--
-  alert(123);
->
- 
+ alert("УХУ!!")
