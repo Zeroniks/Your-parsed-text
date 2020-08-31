374612e2ae60c7fcf8abd8131cf010a65740bbf4
Zeroniks <57189702+Zeroniks@users.noreply.github.com>
2020-08-31 20:23:45 +0700

    dsad

--- Js.js
+++ Js.js
@@ -1,16 +1,22 @@
-"Use script"
-//$('#btn').on('click', function() {
-  //console.log($(this).text());
-//});
+
+$('#btn').on('click', function() {
+  console.log($(this).text());
+});
 
 //let input = document.querySelector('input');
 //console.log(input.value);
 
 
-send.onclick = function() {
-  console.log(this)
-};
+//$send.onclick = function() {
+  //console.log(this)
+//};
 
 //c.click(function(){
   //  console.log(this)
 //  })
+
+//document.forms.publish.onsubmit= function() {
+  //var message = this.message.value;
+  //console.log(message)
+//  return false;
+//};
