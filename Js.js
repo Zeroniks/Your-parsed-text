6b3fa86454a100f196133a4237d6f2b87bca04a3
Zeroniks <57189702+Zeroniks@users.noreply.github.com>
2020-08-31 21:26:18 +0700

    gdcnsxjyrfkjuhgcm

--- Js.js
+++ Js.js
@@ -1,7 +1,7 @@
 
-$('#btn').on('click', function() {
-  console.log($(this).text());
-});
+//$('#btn').on('click', function() {
+//  console.log($(this).text());
+//});
 
 //let input = document.querySelector('input');
 //console.log(input.value);
